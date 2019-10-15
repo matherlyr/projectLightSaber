1dfd9245a3a69736ff6b0672c8245505096af1ff
Rachel K. Matherly <rachel.matherly@centre.edu>
2019-10-15 15:19:14 -0400

    start

--- code.js
+++ code.js
@@ -1,1 +1,26 @@
+var dataPromise= d3.json("https://swapi.co/api/films")
+dataPromise.then(
+    function(filmData)
+    { var info= filmData.results
+        d3.select("h1").text("Welcome to Star Wars Movie Explorer!")
+        console.log("filmData",filmData)
+        displayInfo(info);
+    },
+    function(error)
+    {d3.select("h1").text("No movies.");
+    })
+
+var displayInfo=function(filmData)
+    {console.log(filmData)
+        d3.select("#movieTitles")
+        .append("ol")
+        .selectAll("li")
+        .data(filmData)
+        .enter()
+        .append("li")
+        .text(function(d)
+           {
+            return d.title;  
+            })
+    }
 
